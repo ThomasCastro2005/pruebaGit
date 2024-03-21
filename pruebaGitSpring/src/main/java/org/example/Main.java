94a318aef4eab18646f58f46e3fe35f1a8b5d281
Thomas <castroothomas902@gmail.com>
2024-03-21 18:21:21 -0500

    TERCER commit

--- pruebaGitSpring/src/main/java/org/example/Main.java
+++ pruebaGitSpring/src/main/java/org/example/Main.java
@@ -6,5 +6,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hola mundo");
         System.out.println("dfgdfgfds");
+        System.out.println("diferencia");
     }
 }
